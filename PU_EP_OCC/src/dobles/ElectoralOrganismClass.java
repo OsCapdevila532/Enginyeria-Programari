53081838238de17e69338869d011473f24b5792d
OsCapdevila532 <79273784+OsCapdevila532@users.noreply.github.com>
2023-12-28 18:05:52 +0100

    Paquet services
    Paquet services amb interfaces ElectoralOrganism, LocalService i Scrutiny, i les classes corresponents implementant els metodes.

---
+++ PU_EP_OCC/src/dobles/ElectoralOrganismClass.java
@@ -0,0 +1,31 @@
+package dobles;
+
+import data.Nif;
+import services.ElectoralOrganism;
+import services.exceptions.ConnectException;
+import services.exceptions.NotEnabledException;
+
+public class ElectoralOrganismClass implements ElectoralOrganism{
+    private Nif nif;
+    private boolean canvote;
+    private boolean voted;
+
+    public ElectoralOrganismClass(Nif nif, boolean canvote, boolean voted) {    //Classe doble (dummy) per tests
+        this.nif = nif;
+        this.canvote = canvote;
+        this.voted = voted;
+    }
+
+    @Override
+    public void canVote(Nif nif) throws NotEnabledException, ConnectException {
+        if (this.nif != nif) throw new ConnectException("No s'ha pogut connectar amb el Nif introduit");
+        if (!canvote) throw new NotEnabledException("El votantnt no està al col·legi electoral pertinent");
+        if (voted) throw new NotEnabledException("El votant ja ha excercit el dret a vot");
+    }
+
+    @Override
+    public void disableVoter(Nif nif) throws ConnectException {
+        if (this.nif != nif) throw new ConnectException("No s'ha pogut connectar amb el Nif introduit");
+        this.voted = true;
+    }
+}
